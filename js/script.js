af63c806880082ba7b140c946fc1c14b4ac0759a
Samba Sy <74803028+NewBlack36@users.noreply.github.com>
2023-05-02 08:40:57 +0000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".typing",{
+    strings:["","Designer Web...", "Développeur Web...", "Rédacteur Web...","Community Manager...","Freelancer...", "un GEEK !"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+var naming = new Typed(".naming",{
+    strings:["","New Black", "Samba Sy"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
